849a046b801aed3e3b882452f3caa99cec56105d
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2023-08-17 19:50:09 -0600

    Mute errors from non-existant configs

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -12,7 +12,7 @@ if [ -f ~/.bash_env ]
 end
 
 set script_directory $(dirname (readlink -m (status --current-filename)))
-source "$script_directory/local.config.fish"
+source "$script_directory/local.config.fish" 2> /dev/null
 
 # Start SSH agent on launch
 ssh-activate

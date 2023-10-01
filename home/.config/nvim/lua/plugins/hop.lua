77bc437820031d198eda547059b189828d34855c
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2023-10-01 13:19:29 -0600

    Change hop to use char1

--- home/.config/nvim/lua/plugins/hop.lua
+++ home/.config/nvim/lua/plugins/hop.lua
@@ -1,7 +1,7 @@
 local function init()
   local hop = require('hop')
-  vim.keymap.set('', '<Leader>f', function() hop.hint_words() end, { remap = true })
-  vim.keymap.set('', '<Leader>t', function() hop.hint_words({ hint_offset = -1 }) end, { remap = true })
+  vim.keymap.set('', '<Leader>f', function() hop.hint_char1() end, { remap = true })
+  vim.keymap.set('', '<Leader>t', function() hop.hint_char1({ hint_offset = -1 }) end, { remap = true })
 end
 
 return {

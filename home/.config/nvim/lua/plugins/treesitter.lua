3523ce6441b44ca206ba6e4f41f9b0adc5d57996
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2023-10-24 08:54:01 -0600

    Add treesitter-textsubjects and fix treesitter

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,52 @@
+---@param opts TSConfig
+function config(_, opts)
+  if type(opts.ensure_installed) == "table" then
+    ---@type table<string, boolean>
+    local added = {}
+    opts.ensure_installed = vim.tbl_filter(function(lang)
+      if added[lang] then
+        return false
+      end
+      added[lang] = true
+      return true
+    end, opts.ensure_installed)
+  end
+  require("nvim-treesitter.configs").setup(opts)
+end
+
+
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      init = function()
-        -- disable rtp plugin, as we only need its queries for mini.ai
-        -- In case other textobject modules are enabled, we will load them
-        -- once nvim-treesitter is loaded
-        require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-        load_textobjects = true
-      end,
+  cmd = { "TSUpdateSync" },
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
     },
   },
-  cmd = { "TSUpdateSync" },
+  config = config
 }

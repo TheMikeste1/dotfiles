b5ced92194b928c9b5c7d82021926eaebe823d84
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2023-12-13 07:49:28 -0700

    Fix quickscope colors

--- home/.config/nvim/lua/plugins/quick-scope.lua
+++ home/.config/nvim/lua/plugins/quick-scope.lua
@@ -1,9 +1,10 @@
 return {
   'unblevable/quick-scope',
-  init = function()
+  config = function()
     vim.cmd [[
         highlight QuickScopePrimary   guifg='#a6f25a' gui=underline ctermfg=155 cterm=underline
         highlight QuickScopeSecondary guifg='#5af2f2' gui=underline ctermfg=81  cterm=underline
       ]]
-  end
+  end,
+  priority=1001, -- Needs to be loaded after themes so the highlight is correct
 }

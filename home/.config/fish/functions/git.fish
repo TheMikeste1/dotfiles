08f6707d286083d83de2f84091c21a8c87c79297
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2023-12-30 13:59:56 -0700

    Add printout when using git in a Windows path

--- home/.config/fish/functions/git.fish
+++ home/.config/fish/functions/git.fish
@@ -2,6 +2,7 @@
 
 function git --wraps git --description 'git wrapper to handle Windows paths'
   if pwd -P | grep -q "/mnt/[a-z]/*"
+    echo "Windows path detected, using git.exe"
     git.exe $argv
   else
     command git $argv

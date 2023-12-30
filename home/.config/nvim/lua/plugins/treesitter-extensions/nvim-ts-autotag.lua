8de3df1ea2b4d0e5fbddf4aeb7cb4f0ddebabfd2
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2023-12-30 14:09:47 -0700

    Lazy load nvim-ts-autotag only on certain filetypes

--- home/.config/nvim/lua/plugins/treesitter-extensions/nvim-ts-autotag.lua
+++ home/.config/nvim/lua/plugins/treesitter-extensions/nvim-ts-autotag.lua
@@ -1,6 +1,22 @@
 return {
   'windwp/nvim-ts-autotag',
   cond = not VSCODE,
+  ft = {
+    "astro",
+    "glimmer",
+    "handlebars",
+    "html",
+    "javascript",
+    "jsx",
+    "markdown",
+    "php",
+    "rescript",
+    "svelte",
+    "tsx",
+    "typescript",
+    "vue",
+    "xml",
+  },
   config = function()
     require('nvim-ts-autotag').setup()
   end,

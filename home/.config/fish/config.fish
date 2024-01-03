d9912e7d049fabdbfb203501916713cbdf9993ff
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-02 20:29:03 -0700

    Rework and add plugins to fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,11 @@
+if [ -f ~/.bash_env ]
+    . ~/.bash_env
+end
+
+set script_directory $(dirname (readlink -m (status --current-filename)))
+set fish_function_path $fish_function_path "$script_directory/functions/user"
+set fish_complete_path $fish_complete_path "$script_directory/completions/user"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     function fish_user_key_bindings
@@ -12,28 +20,21 @@ if status is-interactive
     end
 end
 
+export LESS_TERMCAP_mb=\e'[1;31m' # begin bold
+export LESS_TERMCAP_md=\e'[1;36m' # begin blink
+export LESS_TERMCAP_me=\e'[0m' # reset bold/blink
+export LESS_TERMCAP_so=\e'[01;44;33m' # begin reverse video
+export LESS_TERMCAP_se=\e'[0m' # reset reverse video
+export LESS_TERMCAP_us=\e'[1;32m' # begin underline
+export LESS_TERMCAP_ue=\e'[0m' # reset underline
+
+# Start SSH agent on launch
+ssh-activate
+
+starship init fish | source
 
 if [ -f ~/.bash_aliases ]
     . ~/.bash_aliases
 end
 
-if [ -f ~/.bash_env ]
-    . ~/.bash_env
-end
-
-export LESS_TERMCAP_mb=\e'[1;31m'     # begin bold
-export LESS_TERMCAP_md=\e'[1;36m'     # begin blink
-export LESS_TERMCAP_me=\e'[0m'        # reset bold/blink
-export LESS_TERMCAP_so=\e'[01;44;33m' # begin reverse video
-export LESS_TERMCAP_se=\e'[0m'        # reset reverse video
-export LESS_TERMCAP_us=\e'[1;32m'     # begin underline
-export LESS_TERMCAP_ue=\e'[0m'        # reset underline
-
-set script_directory $(dirname (readlink -m (status --current-filename)))
-source "$script_directory/local.config.fish" 2> /dev/null
-
-# Start SSH agent on launch
-ssh-activate
-
-# Starship
-starship init fish | source
+source "$script_directory/local.config.fish" 2>/dev/null

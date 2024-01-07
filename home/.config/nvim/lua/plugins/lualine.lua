dcae102aef9d5cc6f5e9897bcb2348f43cc34a16
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-07 11:34:18 -0700

    Fix up noice

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -4,5 +4,17 @@ return {
   dependencies = {
         {"nvim-tree/nvim-web-devicons", lazy = true}
   },
-  config = true
+  config = function ()
+    require("lualine").setup({
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          }
+        },
+      },
+    })
+  end
 }
--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -16,6 +16,16 @@ local function config()
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "written",
+        },
+        opts = { skip = true },
+      },
+    },
   })
 end
 

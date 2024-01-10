e62e0eca2df915ee1a4b55af83ca9ac07225d37c
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-10 11:23:01 -0700

    Implement conform

--- home/.config/nvim/lua/plugins/ide/conform.lua
+++ home/.config/nvim/lua/plugins/ide/conform.lua
@@ -1,7 +1,18 @@
--- TODO
 return {
 	"stevearc/conform.nvim",
 	cond = not VSCODE,
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<A-F>",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
@@ -10,5 +21,17 @@ return {
 			-- Use a sub-list to run only the first available formatter
 			javascript = { { "prettierd", "prettier" } },
 		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+		},
 	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }

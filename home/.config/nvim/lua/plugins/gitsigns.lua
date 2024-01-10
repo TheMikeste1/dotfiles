48d6844f7e7212aa6f1924db3d5ba895ecf1c872
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-10 14:02:15 -0700

    Fix up gitsigns

--- home/.config/nvim/lua/plugins/gitsigns.lua
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,4 +2,15 @@ return {
 	"lewis6991/gitsigns.nvim",
 	cond = not VSCODE,
 	config = true,
+	opts = {
+		signs = {
+			add = { text = "+" },
+			delete = { text = "-" },
+		},
+		current_line_blame = true,
+		current_line_blame_opts = {
+			delay = 250,
+		},
+		numhl = true,
+	},
 }

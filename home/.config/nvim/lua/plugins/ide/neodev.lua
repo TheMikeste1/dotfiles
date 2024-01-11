e091f831799f4d1970552f22ec130dc4c6551e89
Michael Hegerhorst <michael.hegerhorst@sdl.usu.edu>
2024-01-11 08:54:58 -0700

    Fix neodev with VSCode

--- home/.config/nvim/lua/plugins/ide/neodev.lua
+++ home/.config/nvim/lua/plugins/ide/neodev.lua
@@ -1,1 +1,9 @@
-return { "folke/neodev.nvim", opts = {} }
+return { 
+  "folke/neodev.nvim",
+  cond = not VSCODE,
+  opts = {},
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp"
+  }
+}

eb631bfa2d013e172c01b1fd380484350ad64bfa
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-13 15:07:51 -0700

    Fix up Neotree

--- home/.config/nvim/lua/configure_theme.lua
+++ home/.config/nvim/lua/configure_theme.lua
@@ -15,3 +15,12 @@ vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint'
 vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
 vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
 
+-- Neotree Icons
+vim.fn.sign_define("DiagnosticSignError",
+  {text = " ", texthl = "DiagnosticSignError"})
+vim.fn.sign_define("DiagnosticSignWarn",
+  {text = " ", texthl = "DiagnosticSignWarn"})
+vim.fn.sign_define("DiagnosticSignInfo",
+  {text = " ", texthl = "DiagnosticSignInfo"})
+vim.fn.sign_define("DiagnosticSignHint",
+  {text = "󰌵", texthl = "DiagnosticSignHint"})
--- home/.config/nvim/lua/plugins/ide/neo-tree.lua
+++ home/.config/nvim/lua/plugins/ide/neo-tree.lua
@@ -1,39 +1,3 @@
-require("utilities.file_utilities")
-
-local function open_on_empty()
-	require("neo-tree.command").execute({
-		action = "focus",
-		position = "float",
-		reveal = true,
-	})
-end
-
-local function init()
-	-- vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	-- 	-- Open NeoTree automatically when vim starts if no files were specified or if a directory was specified
-	-- 	callback = function()
-	-- 		if
-	-- 			vim.fn.argc() == 0
-	-- 			or vim.fn.argv()[1] == ""
-	-- 			or (vim.fn.argc() == 1 and IsDirectory(vim.fn.argv()[1]))
-	-- 		then
-	-- 			open_on_empty()
-	-- 			return
-	-- 		end
-	--
-	-- 		-- Filter out arguments, leaving only files and directories
-	-- 		local maybeDirectories = vim.tbl_filter(function(arg)
-	-- 			return not IsDirectory(arg)
-	-- 		end, vim.fn.argv())
-	--
-	-- 		-- Open NeoTree if no files were specified
-	-- 		if vim.tbl_isempty(maybeDirectories) then
-	-- 			open_on_empty()
-	-- 		end
-	-- 	end,
-	-- })
-end
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	cond = not VSCODE,
@@ -44,7 +8,7 @@ return {
 	},
 	keys = {
 		{
-			"<C-E>",
+			"<leader><C-e>",
 			function()
 				require("neo-tree.command").execute({
 					action = "focus",
@@ -55,7 +19,7 @@ return {
 			desc = "Explorer",
 		},
 		{
-			"<C-b>",
+			"<leader><C-b>",
 			function()
 				require("neo-tree.command").execute({
 					action = "show",
@@ -67,7 +31,6 @@ return {
 			desc = "Toggle sideview",
 		},
 	},
-	init = init,
 	opts = {
 		filesystem = {
 			filtered_items = {

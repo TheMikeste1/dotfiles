4974dfaa28dd54bdb1ee0764f446256b7c4d5db7
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-13 21:09:14 -0700

    Fix breakpoints looking awkward

--- home/.config/nvim/lua/configure_theme.lua
+++ home/.config/nvim/lua/configure_theme.lua
@@ -11,23 +11,11 @@ vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#993939" })
 vim.api.nvim_set_hl(0, "DapLogPoint", { fg = "#61afef" })
 vim.api.nvim_set_hl(0, "DapStopped", { fg = "#98c379" })
 
-vim.fn.sign_define(
-	"DapBreakpoint",
-	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-	"DapBreakpointCondition",
-	{ text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-	"DapBreakpointRejected",
-	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-	"DapLogPoint",
-	{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
-)
-vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint" })
+vim.fn.sign_define("DapBreakpointCondition", { text = "ﳁ", texthl = "DapBreakpoint" })
+vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpoint" })
+vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint" })
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped" })
 
 -- Neotree Icons
 vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })

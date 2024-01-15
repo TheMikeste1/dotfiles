50c404e283039d3ce76d0d042200c8f744958e6e
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-15 09:41:00 -0700

    Add keybind to window picker

--- home/.config/nvim/lua/plugins/window-picker.lua
+++ home/.config/nvim/lua/plugins/window-picker.lua
@@ -13,11 +13,26 @@ return {
 				-- filter using buffer options
 				bo = {
 					-- if the file type is one of following, the window will be ignored
-					filetype = { "neo-tree", "neo-tree-popup", "notify" },
+					filetype = { "neo-tree", "neo-tree-popup", "notify", "noice" },
 					-- if the buffer type is one of following, the window will be ignored
 					buftype = { "terminal", "quickfix" },
 				},
 			},
 		})
 	end,
+	keys = {
+		{
+			"<leader>,",
+			function()
+				local window_id = require("window-picker").pick_window()
+				if window_id ~= nil then
+					vim.api.nvim_set_current_win(window_id)
+				end
+			end,
+			mode = {
+				"n",
+				"v",
+			},
+		},
+	},
 }

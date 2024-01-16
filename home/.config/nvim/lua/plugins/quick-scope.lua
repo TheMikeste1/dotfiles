89d220975f160556e296dbc51c587eeb609d3034
Michael Hegerhorst <michael.hegerhorst@sdl.usu.edu>
2024-01-16 13:30:46 -0700

    QuickScope always initializes colors

--- home/.config/nvim/lua/plugins/quick-scope.lua
+++ home/.config/nvim/lua/plugins/quick-scope.lua
@@ -1,3 +1,7 @@
 return {
 	"unblevable/quick-scope",
+	config = function()
+		vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#a6f25a", underline = true })
+		vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#5af2f2", underline = true })
+	end,
 }

1f2ce749339f5c3fa95771a973709f5bbfef54ad
Michael Hegerhorst <michael.hegerhorst@sdl.usu.edu>
2024-02-06 17:15:23 -0700

    Use pip to install ansible

--- init.sh
+++ init.sh
@@ -3,11 +3,7 @@ set -e
 
 if ! [ -x "$(command -v ansible)" ]; then
     echo "Installing Ansible..."
-    sudo apt update
-    sudo apt install software-properties-common
-    sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible
+    pip install ansible
 fi
 
 ansible-playbook --ask-become-pass initialize.yaml
-

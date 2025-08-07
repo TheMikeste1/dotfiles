7db49e75fea166379184070d5f5c7b76c4beb3b3
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2025-08-06 19:31:45 -0600

    Autoinstall pip

--- init.sh
+++ init.sh
@@ -2,8 +2,18 @@
 set -e
 
 if ! [ -x "$(command -v ansible)" ]; then
+    if ! [ -x "$(command -v pip)" ]; then
+        if [ "$(which python3)" = /usr/bin/python3 ]; then
+            echo "Installing pip for system..."
+            sudo apt-get install python3-pip
+        else
+            echo "Unable to install ansible. Pip is not installed for $(which python3)."
+            exit 1
+        fi
+    fi
+
     echo "Installing Ansible..."
-    pip install ansible
+    python3 -m pip install --break-system-packages ansible
     ansible-galaxy collection install community.general
 fi
 
